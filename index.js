ecce656633ce1fe1e7ceb1dc3411d67f68a4dc1b
shahfahad19 <63948421+shahfahad19@users.noreply.github.com>
2022-05-25 09:10:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -52,13 +52,9 @@ app.get("/shows", (req, res) => {
 
 app.get("/videos", (req, res) => {
 	const url = req.query.url.replaceAll(" ", "%20") || "";
-	let src2 = url;
-	if (src2.includes("nxt")){
-		src2 = src2.replace(/[0-9]/g, ''). replace('https://akwam.to/shows/', '').replace("/wwe-nxt", "");
-		src = src.replace(src2[0], "%20"); 
-	} 
+	
 	//Getting the url shortner page
-	request(src2, function (error, response, html) {
+	request(url, function (error, response, html) {
 		let shows = [];
 		if (!error) {
 			var $ = cheerio.load(html);
