e2095a402a3358aecafabed7aee17f78777d2c2b
shahfahad19 <sfahad4280@gmail.com>
2022-05-25 16:11:29 +0500

    some changes

--- frontend/script.js
+++ frontend/script.js
@@ -1,41 +1,40 @@
 window.onload = function () {
-	Swal.fire({
-		html: 'Loading the shows',
-		allowOutsideClick: false,
-		didOpen: () => {
-			Swal.showLoading()
-		}
-	})
+    Swal.fire({
+        html: "Loading the shows",
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
     loadData();
     $("#back").hide();
-	$("#vid").hide();
-}
-let page =0;
+    $("#vid").hide();
+};
+let page = 0;
 
 const loadData = () => {
-	page++;
-	$("#more").hide();
-	$.ajax({
-		url: "http://localhost:9000/shows?page="+page,
-		success: function (data) {
-				Swal.close();
-		    	$("#more").show();
-				showData(data);
-			},
-			error: function(){
-				Swal.fire({
-				icon: 'error',
-				text: 'Something went wrong!',
-			})
-		}
-		
-	});
-} 
+    page++;
+    $("#more").hide();
+    $.ajax({
+        url: "https://wrestlingapi.vercela.app/?page=" + page,
+        success: function (data) {
+            Swal.close();
+            $("#more").show();
+            showData(data);
+        },
+        error: function () {
+            Swal.fire({
+                icon: "error",
+                text: "Something went wrong!",
+            });
+        },
+    });
+};
 
 const showData = (data) => {
-	$("#playerContainer").html("");
-	for (let i = 0; i<data.length;i++) {
-		$('#main').append(`
+    $("#playerContainer").html("");
+    for (let i = 0; i < data.length; i++) {
+        $("#main").append(`
 			<div class="col-6 col-sm-4 col-md-3 col-lg-2 px-1">
 				<div class="" onclick="startPlayer('${data[i].link}', '${data[i].image}', '${data[i].title}')">
 					<img class="w-100 rounded" src="${data[i].image}" />
@@ -43,72 +42,77 @@ const showData = (data) => {
 				</div>
 			</div>
 			`);
-	}
-}
+    }
+};
 
 const startPlayer = (src, img, title) => {
-	title = title.replace(/[0-9]/g, '').replaceAll('.', '').replaceAll('  ', '');
-	Swal.fire({
-		html: 'Loading '+ title,
-		allowOutsideClick: false,
-		didOpen: () => {
-			Swal.showLoading()
-		}
-	})
-	$.ajax({
-		url: "http://localhost:9000/videos?url="+src,
-		success: function (data) {
-			window.scrollTo(0,0);
-			$("#vid").show();
-			$("#main").hide();
-			$("#more").hide();
-			Swal.close();
-			let source = data.videos.length == 1 ? data.videos[0].link : data.videos[1].link;
-			
-			if (data.videos.length ==1) {
-				$("#qualities").hide();
-			}
-			else {
-				$("#sd").attr("onclick", `playVideo('${data.videos[1].link}')`);
-				$("#sd").attr(data.videos[1].size);
+    title = title
+        .replace(/[0-9]/g, "")
+        .replaceAll(".", "")
+        .replaceAll("  ", "");
+    Swal.fire({
+        html: "Loading " + title,
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+    $.ajax({
+        url: "https://wrestlingapi.vercel.app/watch?v=" + src,
+        success: function (data) {
+            window.scrollTo(0, 0);
+            $("#vid").show();
+            $("#main").hide();
+            $("#more").hide();
+            Swal.close();
+            let source =
+                data.videos.length == 1
+                    ? data.videos[0].link
+                    : data.videos[1].link;
 
-				$("#hd").attr("onclick", `playVideo('${data.videos[0].link}')`);
-				$("#hd").attr(data.videos[0].size);
-				
+            if (data.videos.length == 1) {
+                $("#qualities").hide();
+            } else {
+                $("#sd").attr("onclick", `playVideo('${data.videos[1].link}')`);
+                $("#sd").attr(data.videos[1].size);
 
-			}
-			
-			playVideo(source);
-		},
-		error: function (err) {
-			Swal.close();
-			Swal.fire({
-				icon: 'error',
-				title: 'Error',
-				text: 'This show is not available!',
-			})
-		}
-	});
-}
+                $("#hd").attr("onclick", `playVideo('${data.videos[0].link}')`);
+                $("#hd").attr(data.videos[0].size);
+            }
+
+            playVideo(source);
+        },
+        error: function (err) {
+            Swal.close();
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "This show is not available!",
+            });
+        },
+    });
+};
 
 const playVideo = (video) => {
-	$("#dl").attr("href", video);
-	$("#playerContainer").html("");
-	const config = {
-		autoplay: true, 
-		sources: [{
-			type: 'mp4',
-			src: video
-		}]
-	};
+    $("#dl").attr("href", video);
+    $("#playerContainer").html("");
+    const config = {
+        autoplay: true,
+        sources: [
+            {
+                type: "mp4",
+                src: video,
+            },
+        ],
+    };
 
-	const element = document.getElementById('playerContainer');
-	const player = IndigoPlayer.init(element, config);
-}
+    const element = document.getElementById("playerContainer");
+    const player = IndigoPlayer.init(element, config);
+};
 
-const goBack =() => {
-	$("#playerContainer").html("");
-     $("#vid").hide();
-     $("#main").show();
-     $("#more").show();
-} 
+const goBack = () => {
+    $("#playerContainer").html("");
+    $("#vid").hide();
+    $("#main").show();
+    $("#more").show();
+};
--- index.js
+++ index.js
@@ -1,136 +1,185 @@
-
 // create an express app
 const express = require("express");
 const request = require("request");
 const cheerio = require("cheerio");
 const app = express();
 const cors = require("cors");
-app.use(cors({origin: "*"}));
+app.use(cors({ origin: "*" }));
 
 // use the express-static middleware
 app.use(express());
 
-//routing
+// define the first route
+app.get("/", function (req, res) {
+    let page = req.query.page || 1;
 
-app.get("/shows", (req, res) => {
-	let page = req.query.page || "1";
-	page = parseInt(page) * 2;
-	console.log(page);
-	let url =
-		"https://akwam.to/shows?page="+page
+    page = page == 1 ? parseInt(page) : parseInt(page) * 2;
+    let url = "https://akwam.to/shows?page=" + page;
 
-	request(url, function (error, response, html) {
-		let shows = [];
-		if (!error) {
-			var $ = cheerio.load(html);
-			data = $(".widget-body").html();
-			$(".widget-body")
-				.children()
-				.each(function (index, item) {
-					let link = $(item).children("div").children(".entry-body").children("h3").children("a").attr("href");
-					let title = $(item).children("div").children(".entry-body").children("h3").children("a").text();
-					let image = $(item).children("div").children(".entry-image").children("a").children("picture").children("img").attr("data-src");
-					title = title.includes("Smackdown") ? title.replace("Friday ", "").replace("Night ", "") : title;
-					title = title.includes("Raw") ? title.replace("Monday ", "").replace("Night ", "") : title;
+    request(url, function (error, response, html) {
+        let shows = [];
+        if (!error) {
+            var $ = cheerio.load(html);
+            data = $(".widget-body").html();
+            $(".widget-body")
+                .children()
+                .each(function (index, item) {
+                    let link = $(item)
+                        .children("div")
+                        .children(".entry-body")
+                        .children("h3")
+                        .children("a")
+                        .attr("href");
+                    let title = $(item)
+                        .children("div")
+                        .children(".entry-body")
+                        .children("h3")
+                        .children("a")
+                        .text();
+                    let image = $(item)
+                        .children("div")
+                        .children(".entry-image")
+                        .children("a")
+                        .children("picture")
+                        .children("img")
+                        .attr("data-src");
+                    title = title.includes("Smackdown")
+                        ? title.replace("Friday ", "").replace("Night ", "")
+                        : title;
+                    title = title.includes("Raw")
+                        ? title.replace("Monday ", "").replace("Night ", "")
+                        : title;
 
-					if(title.includes("WWE") || title.includes("AEW")) {
-						shows.push({
-							title: title,
-							image: image,
-							link: link
-						})
-					}
-				});
-			url = "https://akwam.to/shows?page=" + (page + 1);
-			request(url, function (error, response, html) {
-				if (!error) {
-					var $ = cheerio.load(html);
-					data = $(".widget-body").html();
-					$(".widget-body")
-						.children()
-						.each(function (index, item) {
-							let link = $(item).children("div").children(".entry-body").children("h3").children("a").attr("href");
-							let title = $(item).children("div").children(".entry-body").children("h3").children("a").text();
-							let image = $(item).children("div").children(".entry-image").children("a").children("picture").children("img").attr("data-src");
-							title = title.includes("Smackdown") ? title.replace("Friday ", "").replace("Night ", "") : title;
-							title = title.includes("Raw") ? title.replace("Monday ", "").replace("Night ", "") : title;
+                    if (title.includes("WWE") || title.includes("AEW")) {
+                        shows.push({
+                            title: title,
+                            image: image,
+                            link: link,
+                        });
+                    }
+                });
+            url = "https://akwam.to/shows?page=" + (page + 1);
+            request(url, function (error, response, html) {
+                if (!error) {
+                    var $ = cheerio.load(html);
+                    data = $(".widget-body").html();
+                    $(".widget-body")
+                        .children()
+                        .each(function (index, item) {
+                            let link = $(item)
+                                .children("div")
+                                .children(".entry-body")
+                                .children("h3")
+                                .children("a")
+                                .attr("href");
+                            let title = $(item)
+                                .children("div")
+                                .children(".entry-body")
+                                .children("h3")
+                                .children("a")
+                                .text();
+                            let image = $(item)
+                                .children("div")
+                                .children(".entry-image")
+                                .children("a")
+                                .children("picture")
+                                .children("img")
+                                .attr("data-src");
+                            title = title.includes("Smackdown")
+                                ? title
+                                      .replace("Friday ", "")
+                                      .replace("Night ", "")
+                                : title;
+                            title = title.includes("Raw")
+                                ? title
+                                      .replace("Monday ", "")
+                                      .replace("Night ", "")
+                                : title;
 
-							if (title.includes("WWE") || title.includes("AEW")) {
-								shows.push({
-									title: title,
-									image: image,
-									link: link
-								})
-							}
-						});
-					res.send(shows);
-					
-				}
-			});
-		}
-	});
+                            if (
+                                title.includes("WWE") ||
+                                title.includes("AEW")
+                            ) {
+                                shows.push({
+                                    title: title,
+                                    image: image,
+                                    link: link,
+                                });
+                            }
+                        });
+                    res.send(shows);
+                }
+            });
+        }
+    });
 });
 //document.querySelector("#show-episodes > div > div > div:nth-child(5) > div > div > a")
 
-app.get("/videos", (req, res) => {
-	let url = req.query.url.replaceAll(" ", "%20") || "";
-	let src = url;
-	if (src.includes("nxt")){
-		src = src.replace(/[0-9]/g, ''). replace('https://akwam.to/shows/', '').replace("/wwe-nxt", "");
-		url = url.replace(src[0], "%20"); 
-	}
+app.get("/watch", (req, res) => {
+    let url = req.query.v.replaceAll(" ", "%20") || "";
+    let src = url;
+    if (src.includes("nxt")) {
+        src = src
+            .replace(/[0-9]/g, "")
+            .replace("https://akwam.to/shows/", "")
+            .replace("/wwe-nxt", "");
+        url = url.replace(src[0], "%20");
+    }
 
-	console.log(url);
-	//Getting the url shortner page
-	request(url, function (error, response, html) {
-		let shows = [];
-		if (!error) {
-			var $ = cheerio.load(html);
-			let link = $(".link-show").attr("href");
+    console.log("Video Process Started");
+    //Getting the url shortner page
+    request(url, function (error, response, html) {
+        let shows = [];
+        if (!error) {
+            console.log("1/3");
+            var $ = cheerio.load(html);
+            let link = $(".link-show").attr("href");
+            /* This link can be empty if the page has multiple shows so
+			I have to call another function here if it returns empty */
 
-			console.log("1/3");
-			//Getting Video Page
-			console.log(link);
-			request(link, function (error, response, html) {
-				let shows = [];
-				if (!error) {
-					var $ = cheerio.load(html);
-					let videopagelink = $(".download-link").attr("href");
-					console.log("2/3");
-					//Getting Video Links
-					request(videopagelink, function (error, response, html) {
-						let shows = [];
-						if (!error) {
-							var $ = cheerio.load(html);
-							let videosLinks = [];
-							$("video").children().each(function (index, item) {
-								let link = $(item).attr("src");
-								let size = $(item).attr("size");
-								videosLinks.push({
-									size: size,
-									link: link
-								})
-							});
-							console.log("3/3");
-							res.send({
-								videos: videosLinks
-							});
+            /****** GETTING VIDEO PAGE STARTS *******/
+            request(link, function (error, response, html) {
+                let shows = [];
+                if (!error) {
+                    var $ = cheerio.load(html);
+                    let videopagelink = $(".download-link").attr("href");
+                    console.log("2/3");
 
-						}
-					});
-				}
-			});
-		}
-	});
-})
+                    /****** GETTING VIDEO LINK STARTS *******/
+                    request(videopagelink, function (error, response, html) {
+                        let shows = [];
+                        if (!error) {
+                            var $ = cheerio.load(html);
+                            let videosLinks = [];
+                            $("video")
+                                .children()
+                                .each(function (index, item) {
+                                    let link = $(item).attr("src");
+                                    let size = $(item).attr("size");
+                                    videosLinks.push({
+                                        size: size,
+                                        link: link,
+                                    });
+                                });
+                            console.log("3/3");
+                            res.send({
+                                videos: videosLinks,
+                            });
+                        }
+                    });
+                    /****** GETTING VIDEO LINK ENDS *******/
+                }
+            });
 
-// define the first route
-app.get("/", function (req, res) {
-	res.send("<p>Api is working!</p> ");
+            /****** GETTING VIDEO PAGE ENDS *******/
+        } else {
+            res.send(hehe);
+        }
+    });
 });
 
 // start the server listening for requests
 app.listen(process.env.PORT || 9000, () => {
-	console.clear();
-	console.log("Server is running...")
+    console.clear();
+    console.log("Server is running...");
 });
