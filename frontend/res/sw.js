196c05d05cb0aec2001a5265a7452e221bc7d729
shahfahad19 <sfahad4280@gmail.com>
2022-05-26 12:58:58 +0500

    Making it PWA

---
+++ frontend/res/sw.js
@@ -0,0 +1,41 @@
+// Choose a cache name
+const cacheName = "urdu-news-cache-v2";
+// List the files to precache
+const precacheResources = [
+    "/",
+    "/index.html",
+    "/res/script.js",
+    "/imgs/192.png",
+    "/imgs/384.png",
+    "/imgs/256.png",
+    "/imgs/512.png",
+    "https://code.jquery.com/jquery-3.6.0.min.js",
+    "https://cdn.jsdelivr.net/npm/sweetalert2@11.4.16/dist/sweetalert2.all.min.js",
+    "/res/plyr.css",
+    "/res/plyr.js",
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener("install", (event) => {
+    //console.log('Service worker install event!');
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => cache.addAll(precacheResources))
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    //console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener("fetch", (event) => {
+    //console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
+    );
+});
